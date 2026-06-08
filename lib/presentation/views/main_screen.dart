9e10fab4f6c25bfc7d98f0c1f07407b8ee998ddb
Walker Viani <walkeryslan@gmail.com>
2026-06-08 20:05:17 -0300

    adicionado main_screen

---
+++ lib/presentation/views/main_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget{
+  const MainScreen({super.key});
+  
+  @override
+  State<StatefulWidget> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen>{
+  int _indice = 0;
+
+  final List<Widget> _telas = [
+    const Center(child: Text('Página Inicial')),
+    const Center(child: Text('Adicionar')),
+    const Center(child: Text('Funções')),
+  ];
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Gardien Tech'),
+        backgroundColor: const Color(0xFF2196F3), 
+        foregroundColor: Colors.white,
+      ),
+      body: _telas[_indice],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _indice,
+        onTap: (index){
+          setState(() {
+            _indice = index;
+          });
+        },
+        items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Página Inicial'),
+        BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Adicionar'),
+        BottomNavigationBarItem(icon: Icon(Icons.apps), label: 'Funções')
+      ], 
+      selectedItemColor: const Color(0xFF2196F3),
+      unselectedItemColor: Colors.black,
+      ),
+    );
+  }
+}
