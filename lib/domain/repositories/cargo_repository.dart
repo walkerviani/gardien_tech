4d683e8d6140f069be428363c97cb26e122ebe92
Walker Viani <walkeryslan@gmail.com>
2026-06-17 19:13:02 -0300

    adiciona cargo_repository

---
+++ lib/domain/repositories/cargo_repository.dart
@@ -0,0 +1,9 @@
+import 'package:gardien_tech/domain/entities/cargo.dart';
+
+abstract class CargoRepository {
+  Future<Cargo?> buscarPorId(int id);
+  Future<List<Cargo>> buscarTodos();
+  Future<void> criar(Cargo cargo);
+  Future<void> atualizar(Cargo cargo);
+  Future<void> deletar(int id);
+}
