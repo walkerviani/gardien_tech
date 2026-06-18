394900093cd94c59cc81dc3469f4475d2f6019eb
Walker Viani <walkeryslan@gmail.com>
2026-06-17 21:42:02 -0300

    atualiza cargo para poder receber objetos sem id (criar cargo)

--- lib/domain/entities/cargo.dart
+++ lib/domain/entities/cargo.dart
@@ -1,11 +1,11 @@
 class Cargo{
   // Atributos
-  final int _id;
+  final int? _id;
   String nomeCargo;
 
   // Construtor
   Cargo(this._id, this.nomeCargo);
 
   // Getter
-  int get id => _id;
+  int? get id => _id;
 }
