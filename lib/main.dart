f0e127d648a9993c7470b46ed131e53162e2b785
Walker Viani <walkeryslan@gmail.com>
2026-06-17 21:47:00 -0300

    adiciona injeção de dependência de cargoRepository

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:gardien_tech/data/database.dart';
+import 'package:gardien_tech/data/repositories/cargo_repository_impl.dart';
+import 'package:gardien_tech/domain/repositories/cargo_repository.dart';
 import 'package:gardien_tech/presentation/views/main_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  final database = AppDatabase();
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider<AppDatabase>(
+          create: (_) => database,
+          dispose: (_, db) => db.close(),
+        ),
+        Provider<CargoRepository>(
+          create: (context) => CargoRepositoryImpl(context.read<AppDatabase>()),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,9 +29,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Gardien Tech',
-      home: const MainScreen(),
-    );
+    return MaterialApp(title: 'Gardien Tech', home: const MainScreen());
   }
 }
