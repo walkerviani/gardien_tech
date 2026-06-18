f79dc3e50fb091535a29562fe9a5f632eb275b83
Walker Viani <walkeryslan@gmail.com>
2026-06-17 21:44:48 -0300

    adiciona a tela de cargos

---
+++ lib/presentation/views/cargos_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:gardien_tech/domain/repositories/cargo_repository.dart';
+import 'package:gardien_tech/presentation/viewmodels/cargo_viewmodel.dart';
+import 'package:gardien_tech/presentation/views/gerenciar_cargos_screen.dart';
+import 'package:provider/provider.dart';
+
+class CargosScreen extends StatefulWidget {
+  const CargosScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _CargosScreenState();
+}
+
+class _CargosScreenState extends State<CargosScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cargos'),
+        backgroundColor: const Color(0xFF2196F3),
+        foregroundColor: Colors.white,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => ChangeNotifierProvider(
+                      create: (context) =>
+                          CargoViewModel(context.read<CargoRepository>()),
+                      child: const GerenciarCargosScreen(),
+                    ),
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                foregroundColor: Colors.white,
+                minimumSize: const Size(double.infinity, 70),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4),
+                ),
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.add, size: 30),
+                  SizedBox(width: 20),
+                  Text('Adicionar Novo Cargo', style: TextStyle(fontSize: 18)),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
