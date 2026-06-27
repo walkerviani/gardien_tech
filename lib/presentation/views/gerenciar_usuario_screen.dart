a9b1d134e19127e3232286ae9b1ca22b547e3759
walkerviani <walkeryslan@gmail.com>
2026-06-27 19:25:06 -0300

    adiciona as funcionalidades da tela de criar/editar usuários

--- lib/presentation/views/gerenciar_usuario_screen.dart
+++ lib/presentation/views/gerenciar_usuario_screen.dart
@@ -1,42 +1,162 @@
 import 'package:flutter/material.dart';
+import 'package:gardien_tech/domain/enum/tipo_cargo.dart';
+import 'package:gardien_tech/presentation/viewmodels/usuario_viewmodel.dart';
+import 'package:provider/provider.dart';
 
 class GerenciarUsuarioScreen extends StatefulWidget {
-  const GerenciarUsuarioScreen({super.key});
+  final int? usuarioId;
+  final String? usuarioNome;
+  final int? usuarioidTipoCargo;
+
+  const GerenciarUsuarioScreen({
+    super.key,
+    this.usuarioidTipoCargo,
+    this.usuarioId,
+    this.usuarioNome,
+  });
 
   @override
   State<StatefulWidget> createState() => _GerenciarUsuarioScreenState();
 }
 
 class _GerenciarUsuarioScreenState extends State<GerenciarUsuarioScreen> {
+  final _formKey = GlobalKey<FormState>();
+  late final TextEditingController _nomeController;
+  late final bool isEditing;
+  TipoCargo? _cargoSelecionado;
+
+  @override
+  void initState() {
+    super.initState();
+    isEditing = widget.usuarioId != null;
+    _nomeController = TextEditingController(text: widget.usuarioNome ?? '');
+
+    if (isEditing && widget.usuarioidTipoCargo != null) {
+      _cargoSelecionado = TipoCargo.values
+          .where((cargo) => cargo.id == widget.usuarioidTipoCargo)
+          .firstOrNull;
+    }
+  }
+
+  @override
+  void dispose() {
+    _nomeController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _salvar() async {
+    if (!_formKey.currentState!.validate()) {
+      return; // Finaliza se tiver algum campo inválido no Form
+    }
+    final viewModel = context.read<UsuarioViewmodel>();
+    final nome = _nomeController.text.trim().toUpperCase();
+    final sucesso = await viewModel.salvar(
+      id: widget.usuarioId,
+      nome: nome,
+      idTipoCargo: _cargoSelecionado!.id,
+    );
+    if (!mounted) return;
+
+    if (sucesso) {
+      Navigator.pop(context);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(viewModel.errorMessage ?? 'Erro desconhecido')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final viewModel = context.watch<UsuarioViewmodel>();
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Gerenciar Usuário'),
+        title: Text(isEditing ? 'Editar Usuário' : 'Criar Usuário'),
         backgroundColor: const Color(0xFF2196F3),
         foregroundColor: Colors.white,
       ),
       body: Container(
         padding: EdgeInsets.all(12),
-        child: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Nome',
-              ),
-            ),
-            Align(
-              alignment: AlignmentGeometry.topLeft,
-              child: TextButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              /*
+              Campo do nome
+              */
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: TextFormField(
+                  controller: _nomeController,
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'O nome é obrigatório';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Nome',
+                  ),
                 ),
-                child: const Text('Selecione o cargo'),
               ),
-            ),
-          ],
+              /* 
+              Campo do cargo
+              */
+              Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: DropdownMenuFormField<TipoCargo>(
+                    label: const Text('Cargo'),
+                    validator: (cargo) {
+                      if (cargo == null) {
+                        return 'Selecione um cargo';
+                      }
+                      return null;
+                    },
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    initialSelection: _cargoSelecionado,
+                    dropdownMenuEntries: TipoCargo.values.map((cargo) {
+                      return DropdownMenuEntry(
+                        value: cargo,
+                        label: cargo.nomeCargo,
+                      );
+                    }).toList(),
+                    onSelected: (cargo) {
+                      setState(() => _cargoSelecionado = cargo);
+                    },
+                    onSaved: (cargo) {
+                      _cargoSelecionado = cargo;
+                    },
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: viewModel.isLoading ? null : _salvar,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF4CAF50),
+                    foregroundColor: const Color(0xFFFFFFFF),
+                    minimumSize: const Size(double.infinity, 70),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                  ),
+                  child: viewModel.isLoading
+                      ? const CircularProgressIndicator(
+                          color: Color(0xFF4CAF50),
+                        )
+                      : Text(
+                          isEditing ? 'Atualizar' : 'Salvar',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
