6e0bbdff79f8b0fc564d0a3d513263a011edba65
walkerviani <walkeryslan@gmail.com>
2026-07-06 16:40:17 -0300

    adiciona GerenciarUsuarioViewmodel

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:gardien_tech/domain/repositories/dispositivo_repository.dart';
 import 'package:gardien_tech/domain/repositories/problema_repository.dart';
 import 'package:gardien_tech/domain/repositories/usuario_repository.dart';
 import 'package:gardien_tech/presentation/viewmodels/dispositivo_viewmodel.dart';
+import 'package:gardien_tech/presentation/viewmodels/gerenciar_usuario_viewmodel.dart';
 import 'package:gardien_tech/presentation/viewmodels/problema_viewmodel.dart';
 import 'package:gardien_tech/presentation/viewmodels/problemas_ativos_viewmodel.dart';
 import 'package:gardien_tech/presentation/viewmodels/usuario_viewmodel.dart';
@@ -49,6 +50,10 @@ void main() {
         ChangeNotifierProvider<ProblemasAtivosViewmodel>(
           create: ((context) =>
               ProblemasAtivosViewmodel(context.read<ProblemaRepository>())),
+        ),
+        ChangeNotifierProvider<GerenciarUsuarioViewmodel>(
+          create: ((context) =>
+              GerenciarUsuarioViewmodel(context.read<UsuarioRepository>())),
         ),
       ],
       child: const MyApp(),
---
+++ lib/presentation/viewmodels/gerenciar_usuario_viewmodel.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/foundation.dart';
+import 'package:gardien_tech/domain/entities/usuario.dart';
+import 'package:gardien_tech/domain/repositories/usuario_repository.dart';
+
+class GerenciarUsuarioViewmodel extends ChangeNotifier{
+  final UsuarioRepository _repository;
+
+  GerenciarUsuarioViewmodel(this._repository);
+
+  bool isLoading = false;
+  String? errorMessage;
+
+  Future<bool> salvar({
+    int? id,
+    required String nome,
+    required int idTipoCargo,
+  }) async {
+    isLoading = true;
+    errorMessage = null;
+    notifyListeners();
+
+    if (nome.trim().isEmpty) {
+      errorMessage = 'Nome é obrigatório';
+      isLoading = false;
+      notifyListeners();
+      return false;
+    }
+
+    /*
+    Se já existir um usuário com o mesmo nome e não está editando, 
+    então não permita criar um usuário com o mesmo nome
+    */
+    final jaExiste = await _repository.buscarPorNome(nome);
+    if (jaExiste != null && jaExiste.id != id) {
+      errorMessage = 'Já existe um usuário com esse nome';
+      isLoading = false;
+      notifyListeners();
+      return false;
+    }
+
+    try {
+      Usuario usuario = Usuario(id, idTipoCargo, nome);
+      if (id != null) {
+        await _repository.atualizar(usuario);
+      } else {
+        await _repository.criar(usuario);
+      }
+      return true;
+    } catch (e) {
+      errorMessage = 'Erro ao salvar o usuário';
+      return false;
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+}
