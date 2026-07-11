50659dd4244a72712d0471d41b0582b7256c5be1
walkerviani <walkeryslan@gmail.com>
2026-07-11 12:34:18 -0300

    adicionado lista de dispositivos

--- lib/data/dto/emprestimo_item_com_dispositivo_dto.dart
+++ lib/data/dto/emprestimo_item_com_dispositivo_dto.dart
@@ -1,9 +1,11 @@
+import 'package:gardien_tech/domain/entities/dispositivo.dart';
 import 'package:gardien_tech/domain/entities/emprestimo_dispositivo.dart';
 import 'package:gardien_tech/domain/entities/emprestimo_item.dart';
 
 class EmprestimoItemComDispositivoDTO {
   final EmprestimoItem item;
   final List<EmprestimoDispositivo> dispositivos;
+  final List<Dispositivo> dispositivosObj;
 
-  EmprestimoItemComDispositivoDTO(this.item, this.dispositivos);
+  EmprestimoItemComDispositivoDTO(this.item, this.dispositivos, this.dispositivosObj);
 }
