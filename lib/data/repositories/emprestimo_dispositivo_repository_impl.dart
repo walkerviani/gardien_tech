024f984b3d21afe1f237fb1c8455cef0546507fd
walkerviani <walkeryslan@gmail.com>
2026-07-25 13:27:27 -0300

    adicionado vincularDispositivo e desvincularDispositivo

--- lib/data/repositories/emprestimo_dispositivo_repository_impl.dart
+++ lib/data/repositories/emprestimo_dispositivo_repository_impl.dart
@@ -1,47 +1,115 @@
 import 'package:gardien_tech/data/database.dart';
 import 'package:gardien_tech/data/datasources/emprestimo_dispositivos_datasource.dart';
 import 'package:gardien_tech/domain/entities/emprestimo_dispositivo.dart';
+import 'package:gardien_tech/domain/repositories/dispositivo_repository.dart';
 import 'package:gardien_tech/domain/repositories/emprestimo_dispositivo_repository.dart';
+import 'package:gardien_tech/domain/repositories/emprestimo_item_repository.dart';
 
-class EmprestimoDispositivoRepositoryImpl implements EmprestimoDispositivoRepository{
+class EmprestimoDispositivoRepositoryImpl
+    implements EmprestimoDispositivoRepository {
   final AppDatabase _database;
+  final EmprestimoItemRepository _emprestimoItemRepository;
+  final DispositivoRepository _dispositivoRepository;
 
-  EmprestimoDispositivoRepositoryImpl(this._database);
+  EmprestimoDispositivoRepositoryImpl(
+    this._database,
+    this._dispositivoRepository,
+    this._emprestimoItemRepository,
+  );
 
   @override
   Future<EmprestimoDispositivo?> buscarPorId(int id) async {
-    final emprestimoDispositivo = await (_database.select(_database.emprestimoDispositivos)
-        ..where((ed) => ed.id.equals(id))).getSingleOrNull();
+    final emprestimoDispositivo = await (_database.select(
+      _database.emprestimoDispositivos,
+    )..where((ed) => ed.id.equals(id))).getSingleOrNull();
 
     return emprestimoDispositivo?.toEntity();
   }
 
   @override
-  Future<List<EmprestimoDispositivo>> buscarPorEmprestimoItem(int idEmprestimoItem) async {
-    final emprestimoDispositivos = await (_database.select(_database.emprestimoDispositivos)
-      ..where((ed) => ed.idEmprestimoItem.equals(idEmprestimoItem))).get();
+  Future<List<EmprestimoDispositivo>> buscarPorEmprestimoItem(
+    int idEmprestimoItem,
+  ) async {
+    final emprestimoDispositivos = await (_database.select(
+      _database.emprestimoDispositivos,
+    )..where((ed) => ed.idEmprestimoItem.equals(idEmprestimoItem))).get();
 
     return emprestimoDispositivos.map((ed) => ed.toEntity()).toList();
   }
 
   @override
   Future<void> criar(EmprestimoDispositivo emprestimoDispositivo) async {
-    await _database.into(_database.emprestimoDispositivos).insert(emprestimoDispositivo.toCompanion());
+    await _database
+        .into(_database.emprestimoDispositivos)
+        .insert(emprestimoDispositivo.toCompanion());
   }
 
   @override
   Future<void> atualizar(EmprestimoDispositivo emprestimoDispositivo) async {
     if (emprestimoDispositivo.id == null) {
-      throw ArgumentError('Não é possível atualizar um empréstimo de dispositivo sem id');
+      throw ArgumentError(
+        'Não é possível atualizar um empréstimo de dispositivo sem id',
+      );
     }
 
     await (_database.update(_database.emprestimoDispositivos)
-      ..where((ed) => ed.id.equals(emprestimoDispositivo.id!)))
+          ..where((ed) => ed.id.equals(emprestimoDispositivo.id!)))
         .write(emprestimoDispositivo.toCompanion());
   }
 
   @override
   Future<void> deletar(int id) async {
-    await (_database.delete(_database.emprestimoDispositivos)..where((ed) => ed.id.equals(id))).go();
+    await (_database.delete(
+      _database.emprestimoDispositivos,
+    )..where((ed) => ed.id.equals(id))).go();
+  }
+
+  //  Desfazer a associação. Usado quando o dispositivo errado foi associado
+  //  ou quando o dispositivo é devolvido e precisa ser liberado
+  @override
+  Future<void> desvincularDispositivo(int idEmprestimoDispositivo) async {
+    final empDispositivo = await buscarPorId(idEmprestimoDispositivo);
+    if (empDispositivo == null) {
+      throw ArgumentError('Dispositivo não encontrado');
+    }
+    // Se tiver um dispositivo, marca como disponível
+    if (empDispositivo.idDispositivo != null) {
+      await _dispositivoRepository.marcarDisponivel(
+        empDispositivo.idDispositivo!,
+      );
+    }
+    await deletar(idEmprestimoDispositivo);
+    await _emprestimoItemRepository.diminuirQntSolicitada(
+      empDispositivo.idEmprestimoItem,
+      1,
+    );
+  }
+
+  // Associa um dispositivo a um emprestimo_dispositivo vazio
+  @override
+  Future<void> vincularDispositivo(
+    int idEmprestimoDispositivo,
+    int idDispositivo,
+  ) async {
+    final dispositivo = await _dispositivoRepository.buscarPorId(idDispositivo);
+    final emprestimoDispositivo = await buscarPorId(idEmprestimoDispositivo);
+    final idEmprestimoItem = emprestimoDispositivo?.idEmprestimoItem;
+
+    if (dispositivo == null) {
+      throw ArgumentError('Dispositivo não encontrado');
+    }
+    if (dispositivo.idStatus == 3) {
+      throw ArgumentError('Este dispositivo já está vinculado a um empréstimo');
+    }
+
+    // Apenas vincula, sem validar quantidade
+    await atualizar(
+      EmprestimoDispositivo(
+        idEmprestimoDispositivo,
+        idEmprestimoItem!,
+        idDispositivo: idDispositivo,
+      ),
+    );
+    await _dispositivoRepository.marcarEmUso(idDispositivo);
   }
 }
--- lib/domain/repositories/emprestimo_dispositivo_repository.dart
+++ lib/domain/repositories/emprestimo_dispositivo_repository.dart
@@ -1,9 +1,16 @@
 import 'package:gardien_tech/domain/entities/emprestimo_dispositivo.dart';
 
 abstract class EmprestimoDispositivoRepository {
-  Future<List<EmprestimoDispositivo>> buscarPorEmprestimoItem(int idEmprestimoItem);
+  Future<List<EmprestimoDispositivo>> buscarPorEmprestimoItem(
+    int idEmprestimoItem,
+  );
   Future<EmprestimoDispositivo?> buscarPorId(int id);
   Future<void> criar(EmprestimoDispositivo ed);
   Future<void> atualizar(EmprestimoDispositivo ed);
   Future<void> deletar(int id);
+  Future<void> vincularDispositivo(
+    int idEmprestimoDispositivo,
+    int idDispositivo,
+  );
+  Future<void> desvincularDispositivo(int idEmprestimoDispositivo);
 }
