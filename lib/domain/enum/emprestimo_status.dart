bf0f1aefed7b139b9d6074a6fa6c2214926b3133
walkerviani <walkeryslan@gmail.com>
2026-07-28 16:48:32 -0300

    melhorado texto dos enums

--- lib/domain/enum/emprestimo_status.dart
+++ lib/domain/enum/emprestimo_status.dart
@@ -1,7 +1,10 @@
 enum EmprestimoStatus {
   ativo(1, 'ATIVO'),
-  emObservacao(2, 'EM_OBSERVACAO'), // Caso os dispositivos tenham sido devolvidos sem ter marcado como concluído
-  concluido(3, 'CONCLUIDO');
+  emObservacao(
+    2,
+    'EM OBSERVAÇÃO',
+  ), // Caso os dispositivos tenham sido devolvidos sem ter marcado como concluído
+  concluido(3, 'CONCLUÍDO');
 
   final int id;
   final String nomeStatus;
