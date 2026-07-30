fc450a51e79ee7ad41592088df2a4cff5125114c
walkerviani <walkeryslan@gmail.com>
2026-07-29 21:24:07 -0300

    adicionado pagina de prévia do pdf

---
+++ lib/presentation/views/visualizar_pdf_screen.dart
@@ -0,0 +1,37 @@
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:printing/printing.dart';
+import 'package:pdf/pdf.dart';
+
+class VisualizarPdfScreen extends StatelessWidget {
+  final String tituloPagina;
+  final Future<Uint8List> Function(PdfPageFormat) criarPdf;
+
+  const VisualizarPdfScreen(this.tituloPagina, this.criarPdf, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(
+        primaryColor: const Color(0xFF2196F3),
+        progressIndicatorTheme: const ProgressIndicatorThemeData(
+          color: Color(0xFF2196F3),
+        ),
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(tituloPagina),
+          backgroundColor: const Color(0xFF2196F3),
+          foregroundColor: Colors.white,
+        ),
+        body: PdfPreview(
+          build: criarPdf,
+          allowPrinting: true,
+          allowSharing: true,
+          canChangeOrientation: false,
+          canChangePageFormat: false,
+        ),
+      ),
+    );
+  }
+}
