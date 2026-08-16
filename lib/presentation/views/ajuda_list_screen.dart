e1e9d05c30c22541308609ab765ff87f6e6e2291
walkerviani <walkeryslan@gmail.com>
2026-08-16 15:30:23 -0300

    adicionado menu de ajuda

---
+++ lib/presentation/views/ajuda_list_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:gardien_tech/presentation/views/ajuda_card_screen.dart';
+import 'package:gardien_tech/presentation/views/ajuda_tipo_emprestimo_screen.dart';
+
+class AjudaListScreen extends StatelessWidget {
+  const AjudaListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ajuda e informações'),
+        backgroundColor: const Color(0xFF2196F3),
+        foregroundColor: Colors.white,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(12),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => AjudaCardScreen()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF006dc4),
+                foregroundColor: const Color(0xFFFFFFFF),
+                minimumSize: const Size(double.infinity, 70),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4),
+                ),
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.rectangle_rounded, size: 30),
+                  SizedBox(width: 20),
+                  Text('Cor do cartão', style: TextStyle(fontSize: 18)),
+                ],
+              ),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => AjudaTipoEmprestimoScreen(),
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF006dc4),
+                foregroundColor: const Color(0xFFFFFFFF),
+                minimumSize: const Size(double.infinity, 70),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4),
+                ),
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.type_specimen, size: 30),
+                  SizedBox(width: 20),
+                  Text('Tipo de empréstimo', style: TextStyle(fontSize: 18)),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
