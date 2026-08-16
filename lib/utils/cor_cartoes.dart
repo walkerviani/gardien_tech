6dd882ba8cf6452952fc2e9912b3a6ad739d8fc3
walkerviani <walkeryslan@gmail.com>
2026-08-16 17:14:57 -0300

    renomeado cor_cartoes para cores_gardien

--- lib/utils/cor_cartoes.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class CorCartoes {
-  static const ativo = Colors.green;
-  static const emObservacao = Colors.deepOrangeAccent;
-  static const concluido = Colors.blueGrey;
-  static const semCorrespondencia = Color(0xFFAB1308);
-  static const erro = Colors.black;
-}
---
+++ lib/utils/cores_gardien.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CoresGardien {
+  // Cores app
+  static const azulClaro = Color(0xFF2196F3);
+  static const azulEscuro = Color(0xFF006dc4);
+  static const ciano = Color(0xFF00bcd4);
+  static const verdeClaro = Colors.green;
+  static const vermelhoEscuro = Color(0xFFB00303);
+  static const vermelhoClaro = Colors.red;
+  static const branco = Colors.white;
+  static const cinzaClaro = Colors.blueGrey;
+  static const preto = Color(0xFF000000);
+  static const laranja = Color(0xFFe76f06);
+  static const amarelo = Color(0xFFFfde21);
+  // Status Empréstimo
+  static const statusAtivo = Colors.green;
+  static const statusEmObservacao = Colors.deepOrangeAccent;
+  static const statusConcluido = Colors.blueGrey;
+  static const statusSemCorrespondencia = Color(0xFFAB1308);
+  static const statusErro = Colors.black;
+}
