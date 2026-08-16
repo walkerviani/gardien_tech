aad04573f2553a37779c57872a299c49e5a98b95
walkerviani <walkeryslan@gmail.com>
2026-08-16 16:06:38 -0300

    adicionado tela de ajuda para explicar a cor de cada cartão de empréstimo presente na página inicial

---
+++ lib/presentation/views/ajuda_card_screen.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:gardien_tech/utils/cor_cartoes.dart';
+
+class AjudaCardScreen extends StatelessWidget {
+  const AjudaCardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cor do cartão'),
+        backgroundColor: const Color(0xFF2196F3),
+        foregroundColor: Colors.white,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'O que significa cada cor dos cartões dos empréstimos?',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              'Cada cor representa um status do empréstimo:',
+              style: TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              'ATIVO',
+              style: TextStyle(
+                color: CorCartoes.ativo,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'O empréstimo está aberto para o usuário realizar alterações.',
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              'EM OBSERVAÇÃO',
+              style: TextStyle(
+                color: CorCartoes.emObservacao,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'O empréstimo possui mais de um dia aberto e requer atenção, mas ainda está aberto para o usuário realizar alterações.',
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              'CONCLUÍDO',
+              style: TextStyle(
+                color: CorCartoes.concluido,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'O empréstimo foi finalizado e não pode mais ser alterado, o usuário só tem disponibilidade de ler os dados.',
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              'SEM CORRESPONDÊNCIA',
+              style: TextStyle(
+                color: CorCartoes.semCorrespondencia,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'É quando o empréstimo foi definido como sem correspôndencia, ou seja, o empréstimo está em observação e por algum acaso não há mais como identificar quais eram os dispositivos daquele empréstimo.',
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              'ERRO',
+              style: TextStyle(
+                color: CorCartoes.erro,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Houve alguma falha no sistema ao identificar o status real do empréstimo.',
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
