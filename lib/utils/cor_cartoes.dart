f63bbbc46e7661fa2c70063b670bba60d6a10b93
walkerviani <walkeryslan@gmail.com>
2026-08-16 16:07:41 -0300

    adicionado classe das cores dos cartões/status do empréstimo

---
+++ lib/utils/cor_cartoes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CorCartoes {
+  static const ativo = Colors.green;
+  static const emObservacao = Colors.deepOrangeAccent;
+  static const concluido = Colors.blueGrey;
+  static const semCorrespondencia = Color(0xFFAB1308);
+  static const erro = Colors.black;
+}
