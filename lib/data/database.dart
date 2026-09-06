a746bc5d32089a0e8cdae3d9753fab4003d29bb8
Wendell Ylan Viani <wendellylan@gmail.com>
2026-09-06 17:20:33 -0300

    atualizado schemaVersion para 2 e implementada lógica de migração; otimizada ordenação de dispositivos

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -27,7 +27,7 @@ class AppDatabase extends _$AppDatabase {
   AppDatabase([QueryExecutor? executor]) : super(executor ?? _openConnection());
 
   @override
-  int get schemaVersion => 1;
+  int get schemaVersion => 2;
 
   @override
   MigrationStrategy get migration => MigrationStrategy(
@@ -35,12 +35,23 @@ class AppDatabase extends _$AppDatabase {
       await m.createAll();
     },
     onUpgrade: (m, from, to) async {
-      // Lógica de migração caso existam dados prévios
+      if (from < 2) {
+        await customStatement(
+          'ALTER TABLE emprestimo_itens RENAME TO emprestimo_itens_old;',
+        );
+        await m.createTable(emprestimoItens);
+        await customStatement(
+          'INSERT INTO emprestimo_itens (id, id_emprestimo, id_tipo_dispositivo, qtd_solicitada, qtd_devolvida) '
+          'SELECT id, id_emprestimo, id_tipo_dispositivo, qtd_solicitada, qtd_devolvida '
+          'FROM emprestimo_itens_old;',
+        );
+        await customStatement('DROP TABLE emprestimo_itens_old;');
+      }
     },
     beforeOpen: (details) async {
       await customStatement('PRAGMA foreign_keys = ON;');
       await customStatement('PRAGMA journal_mode = WAL;');
-      
+
       // Índices para otimização de query
       await customStatement(
         'CREATE INDEX IF NOT EXISTS idx_emp_disp_item ON emprestimo_dispositivos(id_emprestimo_item);',
